68a1e98e939e6a96a217f64341718298d2ea64f1
Yussed Sandoval <randy.sandoval@mercadolibre.com.co>
2022-02-17 19:53:13 -0500

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
+import Characters from './Characters'
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <Characters />
+      </div>
     </div>
   );
 }
---
+++ src/components/Character.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Character(props) {
+    return (
+        <div>
+            <p><b>Name: </b> {props.name}</p>
+            <p><b>Genre: </b> {props.genero}</p>
+            <img alt="imagen-personaje" src={props.foto}></img>
+        </div>
+    )
+}
+
+export default Character;
