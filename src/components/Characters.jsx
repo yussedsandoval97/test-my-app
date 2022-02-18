0e0c5243b77059a1f554c5a4c010f3ede52328ec
Yussed Sandoval <randy.sandoval@mercadolibre.com.co>
2022-02-17 19:58:36 -0500

    Add docstring

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Character from "./Character"
 
+/**
+ Methos characters
+ */
+
 function Characters() {
     const [characters, setCharacters] = useState([])
     const [pagesCharacter, setPagesCharacter] = useState(1);
